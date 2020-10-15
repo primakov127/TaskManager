68e4b4607efaec56681b82754bc17d9b5e5864b3
Max Primakov <primakov127@gmail.com>
2020-10-15 22:36:39 +0300

    Add repository classes

---
+++ src/main/java/by/primakov/backend/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package by.primakov.backend.repository;
+
+import by.primakov.backend.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
---
+++ src/main/java/by/primakov/backend/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package by.primakov.backend.repository;
+
+import by.primakov.backend.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
